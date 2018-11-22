7f12de49dedaa2c02e760ff33c8908445fad2767
Jeremy Botha <jeremy.botha@vonage.com>
2018-11-22 12:38:58 +0000

    Cache auth details for 24h

--- app/lib/authenticator.rb
+++ app/lib/authenticator.rb
@@ -11,14 +11,10 @@ class Authenticator
 		Rails.logger.info "Authenticating with remote server"
   	creds = Credential.find_by_number(number)
 
-  	# need error handling here
   	code, auth_res = HttpApi.post(
   		"https://api.#{creds.number}.wa.prod.nexmo.cloud:443/v1/users/login",
   		basic_auth(creds.username, creds.password)
   	)
-
-  	# Rails.logger.info code
-  	# Rails.logger.info auth_res
 
   	if code == :ok
   		token = auth_res["users"].first["token"]
@@ -26,7 +22,6 @@ class Authenticator
   		Rails.logger.error auth_res
   		raise Unauthenticated.new
   	end
-  	# Rails.logger.debug token
 
   	token
   end
@@ -38,10 +33,9 @@ class Authenticator
 
   # fetch the number's token, or authenticate via basic auth and generate a new token if no token is found
   def self.session_token(number)
-		Rails.cache.fetch("tokens/#{number}", :expires_in => 1.minutes) do
+		Rails.cache.fetch("tokens/#{number}", :expires_in => 24.hours) do
 	    authenticate(number)
 	  end
   end
 end
 
-# RestClient::Request.execute(:url => 'https://selfsigned.ssltest.me', :method => :get, :verify_ssl => false)
