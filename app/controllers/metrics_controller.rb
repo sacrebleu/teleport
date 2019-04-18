daae5855840a0616002be3c896b955353046f47a
Jeremy Botha <jeremy.botha@vonage.com>
2019-04-18 21:08:04 +0100

    Merge health check into prometheus metrics

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -14,6 +14,14 @@ class MetricsController < ApplicationController
       res << limit(Stats::Stats.core_stats(params[:number]))
       res << limit(Stats::Stats.db_stats(params[:number]))
 
+      health = <<~EOF
+# HELP liveness check for whatsapp cluster for customer number #{params[:number]}
+# TYPE whatsapp_cluster_health gauge
+whatsapp_cluster_health{customer="#{params[:number]}"} #{limit(Stats::Health.sanity(params[:number]))}
+      EOF
+
+      res << health
+
       render plain: res, status: 200
     rescue Stats::RateLimited
       render plain: { status: :error, message: "Rate limited by server" }
